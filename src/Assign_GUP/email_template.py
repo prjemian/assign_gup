9539b3235a05fec135acc516b8d12d052fade9f5
Pete R Jemian <prjemian@gmail.com>
2015-08-25 15:28:39 -0500

    start to support an email template

---
+++ src/Assign_GUP/email_template.py
@@ -0,0 +1,22 @@
+
+
+class EmailTemplate(object):
+    
+    def __init__(self):
+        pass
+    
+FULL_NAME = 'Ima Reviewer'
+EMAIL = 'reviewer@institution.net'
+PANEL_CHAIR = 'Pete Jemian'
+PRIMARY_PROPOSALS = '11111 22222 33333'
+PROJECT_PROPOSAL_COMMENTS = '''
+This time there are two project proposals:
+   GUP-11111 at 5-ID
+   GUP-22222 at 12ID
+   
+   We've discussed the responsibilities for reviewing project proposals.
+   If you have any questions, call or write me.
+'''
+PRP_DATE = '2015-11-22'
+REVIEW_CYCLE = '2016-1'
+SECONDARY_PROPOSALS = '44444 55555 66666'
