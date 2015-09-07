7ae15e8e8186be0551f47f163d8ff1aef59826ab
jemian <prjemian@gmail.com>
2015-09-07 15:34:36 -0500

    refactor

--- src/Assign_GUP/email_template.py
+++ src/Assign_GUP/email_template.py
@@ -126,56 +126,6 @@ class EmailTemplate(object):
             key_node.text = v.strip()
 
 
-class EmailTextWindow(plainTextEdit.TextWindow):
-    '''
-    plainTextEdit window that remembers its geometry, based on supplied window title
-    '''
-
-    def __init__(self, title, text, settings, parent=None):
-        plainTextEdit.TextWindow.__init__(self, title, text, parent)
-        self.settings = settings
-        self.restoreWindowGeometry()
-    
-    def settingsGroupName(self):
-        '''
-        need a group name in the settings file to save the window geometry, based on window title
-        '''
-        group = str(self.windowTitle()).strip()
-        #group = group.lstrip('email: ')
-        pattern_list = [' ', ':', ';', '(', ')', '[', ']', '.', ',', "'", '"']
-        for pattern in pattern_list:
-            group = group.replace(pattern, '_')
-        return group
-
-    def closeEvent(self, event):
-        self.saveWindowGeometry()
-        event.accept()
-        self.close()
-
-    def moveEvent(self, event):
-        import datetime
-        print datetime.datetime.now()
-        self.saveWindowGeometry()
-        event.accept()
-    
-    def saveWindowGeometry(self):
-        '''
-        remember where the window was
-        '''
-        if self.settings is not None:
-            group = self.settingsGroupName()
-            self.settings.saveWindowGeometry(self, group)
-
-    def restoreWindowGeometry(self):
-        '''
-        put the window back where it was
-        '''
-        if self.settings is not None:
-            group = self.settingsGroupName()
-            self.settings.restoreWindowGeometry(self, group)
-
-
 if __name__ == '__main__':
     et = EmailTemplate()
     print et.mail_merge(**REVIEWER_FIELDS)
-    
