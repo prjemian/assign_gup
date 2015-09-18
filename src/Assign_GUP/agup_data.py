df88b2145a75ec93acf361b54abb1e2a3e528e1b
Pete R Jemian <prjemian@gmail.com>
2015-09-18 07:14:22 -0500

    refs #11

--- src/Assign_GUP/agup_data.py
+++ src/Assign_GUP/agup_data.py
@@ -5,9 +5,13 @@ Data model for a review session: proposals, reviewers, topics, and analyses
 
 import datetime
 from lxml import etree
-import os, sys
 import history
-from PyQt4 import QtCore
+import os, sys
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+if on_rtd:
+    from mock_PyQt4 import QtCore
+else:
+    from PyQt4 import QtCore
 import StringIO
 import traceback
 
