f022283889b8de4eb62ddd77646470c0a1c30fda
jemian <prjemian@gmail.com>
2016-01-13 13:05:35 -0600

    fixes #23: LICENSE window does not display

--- setup.py
+++ setup.py
@@ -40,7 +40,8 @@ setup (name             = Assign_GUP.__package_name__,        # Assign_GUP
                            # do not really need to package this mock
                            'Assign_GUP.mock_PyQt4',
                            ],
-       package_data     = dict(Assign_GUP=['resources/*', ]),
+       package_data     = dict(Assign_GUP=['resources/*', 
+                                           'LICENSE', ]),
        classifiers      = Assign_GUP.__classifiers__,
        entry_points={
             # create & install console_scripts in <python>/bin
