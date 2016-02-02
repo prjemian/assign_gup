13441728ecf6ae6c9811318044ab9db02059804a
jemian <prjemian@gmail.com>
2016-02-01 22:08:47 -0600

    special handling for RTD

--- src/Assign_GUP/__init__.py
+++ src/Assign_GUP/__init__.py
@@ -55,4 +55,9 @@ from _version import get_versions
 __version__ = get_versions()['version']
 del get_versions
 
+import os
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+if on_rtd:
+    # special handling for readthedocs.org, remove distracting info
+    __version__ = __version__.split('+')[0]
 __release__             = __version__
