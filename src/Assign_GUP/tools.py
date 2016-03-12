63747f27f2f6773ca99bf10b78a72737b6e8f294
jemian <prjemian@gmail.com>
2016-03-11 21:25:56 -0600

    fixes #57: codepoint conflicts resolved

--- src/Assign_GUP/tools.py
+++ src/Assign_GUP/tools.py
@@ -11,7 +11,7 @@ CODEPOINT_LIST = ('cp1251', 'cp1252', 'utf8', 'iso-8859-1')
 
 
 def text_decode(source):
-    '''try decoding``source`` with various known codepoints to unicode'''
+    '''try decoding ``source`` with various known codepoints to unicode'''
     for encoding in CODEPOINT_LIST:  # walk through a list of codepoints
         try:
             return source.decode(encoding, 'replace')
